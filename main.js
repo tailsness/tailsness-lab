8888c78a3d51b4d359a6915e618701c7941554a2
tailsness <tailsness@me.com>
2014-07-02 20:28:30 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+chrome.app.runtime.onlaunched.add:istener(function() {
+  chrome.appwindow.create(create'index.html', {
+    bounds: {
+      width: 500,
+      height: 309
+      
+      }
+      });
+      });
+      
